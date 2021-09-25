459a8b1a0ee07763b3fa668889aa62d8d4878a97
metalicn20 <metalicn20@gmail.com>
2021-09-25 16:57:12 +0330

    Add interpreter object to ethereum chain class

--- src/main/kotlin/co/nilin/opex/chainscan/chain/EthereumChain.kt
+++ src/main/kotlin/co/nilin/opex/chainscan/chain/EthereumChain.kt
@@ -2,11 +2,13 @@ package co.nilin.opex.chainscan.chain
 
 import co.nilin.opex.chainscan.model.Transfer
 import co.nilin.opex.chainscan.service.Chain
+import co.nilin.opex.chainscan.service.Interpreter
 import org.springframework.beans.factory.annotation.Value
 import org.web3j.protocol.Web3j
+import org.web3j.protocol.core.methods.response.EthBlock
 import org.web3j.protocol.http.HttpService
 
-class EthereumChain : Chain {
+class EthereumChain(private val interpreter: Interpreter<EthBlock.TransactionObject>) : Chain {
     @Value("\${blockchain.url}")
     private lateinit var url: String
     private val web3j: Web3j = Web3j.build(HttpService(url))
