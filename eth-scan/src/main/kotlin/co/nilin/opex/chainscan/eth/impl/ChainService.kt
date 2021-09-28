43f8eb61ad3370a0d4b876fba32c5269c5ba1913
metalicn20 <metalicn20@gmail.com>
2021-09-28 11:42:27 +0330

    Fix web3j field

--- eth-scan/src/main/kotlin/co/nilin/opex/chainscan/eth/impl/ChainService.kt
+++ eth-scan/src/main/kotlin/co/nilin/opex/chainscan/eth/impl/ChainService.kt
@@ -3,9 +3,9 @@ package co.nilin.opex.chainscan.eth.impl
 import co.nilin.opex.chainscan.core.model.Transfer
 import co.nilin.opex.chainscan.core.spi.Chain
 import co.nilin.opex.chainscan.core.spi.Interpreter
-import co.nilin.opex.chainscan.eth.interpreter.EthereumInterpreter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.web3j.protocol.Web3j
@@ -16,14 +16,14 @@ import org.web3j.protocol.http.HttpService
 class ChainService(private val interpreter: Interpreter<EthBlock.TransactionObject>) : Chain {
     @Value("\${blockchain.url}")
     private lateinit var url: String
-    private val web3j: Web3j = Web3j.build(HttpService(url))
+    private val web3j: Web3j by lazy { Web3j.build(HttpService(url)) }
 
     override suspend fun getTransfers(startBlock: Long, endBlock: Long, addresses: List<String>): List<Transfer> {
         val transfers = mutableListOf<Transfer>()
-        runBlocking {
+        coroutineScope {
             val times = (endBlock - startBlock).toInt()
             repeat(times) { i ->
-                launch {
+                launch(Dispatchers.IO) {
                     val blockNumber = { (startBlock + i).toString() }
                     val transactions = web3j.ethGetBlockByNumber(blockNumber, true).send().block.transactions
                     transactions.forEach {
