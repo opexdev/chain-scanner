b5f64ee98271f55146565eb4f1f860df03246295
metalicn20 <metalicn20@gmail.com>
2021-09-28 17:42:14 +0330

    Fix multi-module structure

--- gateway/src/main/kotlin/co/nilin/opex/chainscan/gateway/GatewayApplication.kt
+++ gateway/src/main/kotlin/co/nilin/opex/chainscan/gateway/GatewayApplication.kt
@@ -2,10 +2,11 @@ package co.nilin.opex.chainscan.gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
 
 @SpringBootApplication
 class GatewayApplication
 
 fun main(args: Array<String>) {
-	runApplication<GatewayApplication>(*args)
+    runApplication<GatewayApplication>(*args)
 }
