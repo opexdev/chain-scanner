a146122a182cc3770757e9b9793aab44bee94d9d
metalicn20 <metalicn20@gmail.com>
2021-09-29 10:54:24 +0330

    Cleanup imports

--- eth-scan/src/main/kotlin/co/nilin/opex/chainscan/eth/config/AppConfig.kt
+++ eth-scan/src/main/kotlin/co/nilin/opex/chainscan/eth/config/AppConfig.kt
@@ -1,8 +1,5 @@
 package co.nilin.opex.chainscan.eth.config
 
-import co.nilin.opex.chainscan.eth.impl.ChainService
-import co.nilin.opex.chainscan.eth.interpreter.EthereumInterpreter
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
--- gateway/src/main/kotlin/co/nilin/opex/chainscan/gateway/GatewayApplication.kt
+++ gateway/src/main/kotlin/co/nilin/opex/chainscan/gateway/GatewayApplication.kt
@@ -2,7 +2,6 @@ package co.nilin.opex.chainscan.gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
 
 @SpringBootApplication
 class GatewayApplication
