281ea9a5228d8d50c8389fd9f7973bd127b096d0
metalicn20 <metalicn20@gmail.com>
2021-09-29 22:30:31 +0330

    Improve interpret class

--- eth-scan/src/main/kotlin/co/nilin/opex/chainscan/eth/impl/EthereumInterpreter.kt
+++ eth-scan/src/main/kotlin/co/nilin/opex/chainscan/eth/impl/EthereumInterpreter.kt
@@ -4,24 +4,33 @@ import co.nilin.opex.chainscan.core.model.Transfer
 import co.nilin.opex.chainscan.core.spi.Interpreter
 import org.springframework.stereotype.Component
 import org.web3j.protocol.core.methods.response.EthBlock
-import kotlin.math.pow
+import java.math.BigDecimal
 
-const val ERC20_TRANSFER_METHOD_ID = "0xa9059cbb000000000000000000000000"
-const val ETH_TRANSFER_METHOD_ID = "0x"
+const val ERC20_TRANSFER_METHOD_SIG = "0xa9059cbb000000000000000000000000"
+const val ETH_TRANSFER_METHOD_SIG = "0x"
 
 @Component
 class EthereumInterpreter : Interpreter<EthBlock.TransactionObject> {
+    private fun isAssetTransfer(input: String) = input == ETH_TRANSFER_METHOD_SIG
+    private fun isTokenTransfer(input: String) = input.length == 138 && input.startsWith(ERC20_TRANSFER_METHOD_SIG)
+
+    private fun parseAssetTransfer(tx: EthBlock.TransactionObject): Transfer {
+        if (!isAssetTransfer(tx.input)) throw IllegalArgumentException()
+        val amount = BigDecimal(tx.value, 18)
+        return Transfer(tx.hash, tx.from, tx.to, false, null, amount)
+    }
+
+    private fun parseTokenTransfer(tx: EthBlock.TransactionObject): Transfer {
+        if (!isTokenTransfer(tx.input)) throw IllegalArgumentException()
+        val receiver = "0x${tx.input.substring(34, 74)}"
+        val amount = BigDecimal(tx.input.substring(74).toLong(16).toBigInteger(), 18)
+        return Transfer(tx.hash, tx.from, receiver, true, tx.to, amount)
+    }
+
     override fun interpret(tx: EthBlock.TransactionObject): Transfer? {
         return when {
-            tx.input == ETH_TRANSFER_METHOD_ID -> {
-                val amount = tx.value.toBigDecimal().divide((10.0).pow(18).toBigDecimal())
-                Transfer(tx.hash, tx.from, tx.to, false, null, amount)
-            }
-            tx.input.startsWith(ERC20_TRANSFER_METHOD_ID) -> {
-                val receiver = "0x${tx.input.substring(34, 74)}"
-                val amount = tx.input.substring(74).toLong(16).toBigDecimal().divide((10.0).pow(18).toBigDecimal())
-                Transfer(tx.hash, tx.from, receiver, true, tx.to, amount)
-            }
+            isAssetTransfer(tx.input) -> parseAssetTransfer(tx)
+            isTokenTransfer(tx.input) -> parseTokenTransfer(tx)
             else -> null
         }
     }
