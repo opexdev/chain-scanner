5256b099931536aecac66c6d77b4ba166ce66df5
Peyman <marchosiax@gmail.com>
2022-04-22 13:36:11 +0430

    Limit number of requested blocks

--- eth-scan/src/main/kotlin/co/nilin/opex/chainscan/eth/impl/ChainService.kt
+++ eth-scan/src/main/kotlin/co/nilin/opex/chainscan/eth/impl/ChainService.kt
@@ -22,19 +22,22 @@ class ChainService(
     private val logger = LoggerFactory.getLogger(ChainService::class.java)
 
     override suspend fun getTransfers(startBlock: Long, endBlock: Long?, addresses: List<String>?): TransfersResult {
-        logger.info("requested blocks: startBlock=$startBlock, endBlock=$endBlock")
+        logger.info("Requested blocks: startBlock=$startBlock, endBlock=$endBlock")
+
         val transfers = mutableListOf<Transfer>()
         var last: Long
         coroutineScope {
             val networkBlockHeight = web3j.ethBlockNumber().send().blockNumber.toLong()
             last = endBlock ?: networkBlockHeight
-            val first = if (startBlock == 0L || startBlock >= last) last - 10 else startBlock
+            val first = if (startBlock == 0L || startBlock >= last || last - startBlock > 10) last - 10 else startBlock
 
-            logger.info("start fetching ethereum transfers: startBlock=$first, endBlock=$last")
+            logger.info("Start fetching ethereum transfers: startBlock=$first, endBlock=$last")
             for (i in first until last + 1) {
                 launch(Dispatchers.IO) {
                     val blockNumber = DefaultBlockParameterNumber(i)
                     val block = web3j.ethGetBlockByNumber(blockNumber, true).send().block
+                    logger.info("Fetched block $i with ${block.transactions.size} transactions")
+
                     block?.transactions?.forEach {
                         val tx = it as EthBlock.TransactionObject
                         val transfer = interpreter.interpret(tx)
@@ -47,6 +50,8 @@ class ChainService(
                 }
             }
         }
+
+        logger.info("Finished fetching transactions: lastBlock=$last transfers=${transfers.size}")
         return TransfersResult(last, transfers)
     }
 }
