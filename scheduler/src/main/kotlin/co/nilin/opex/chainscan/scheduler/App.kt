2bd2329eea5f2636e251048620ddc9f370dabcab
metalicn20 <metalicn20@gmail.com>
2022-05-25 10:23:10 +0430

    Add scheduler blank module

---
+++ scheduler/src/main/kotlin/co/nilin/opex/chainscan/scheduler/App.kt
@@ -0,0 +1,11 @@
+package co.nilin.opex.chainscan.scheduler
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class App
+
+fun main() {
+    runApplication<App>()
+}
