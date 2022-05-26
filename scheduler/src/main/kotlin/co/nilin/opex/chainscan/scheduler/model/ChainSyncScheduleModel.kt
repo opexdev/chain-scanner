143fed6bbe37fe27ebc240b0c91aad19265c463e
ebrahimmfadae <ebrahimmfadae@gmail.com>
2022-05-27 00:11:18 +0430

    Improve scheduler

--- scheduler/src/main/kotlin/co/nilin/opex/chainscan/scheduler/model/ChainSyncScheduleModel.kt
+++ scheduler/src/main/kotlin/co/nilin/opex/chainscan/scheduler/model/ChainSyncScheduleModel.kt
@@ -1,14 +1,14 @@
 package co.nilin.opex.chainscan.scheduler.model
 
 import org.springframework.data.annotation.Id
-import org.springframework.data.relational.core.mapping.Column
 import org.springframework.data.relational.core.mapping.Table
 import java.time.LocalDateTime
 
 @Table("chain_sync_schedules")
 data class ChainSyncScheduleModel(
-    @Id @Column("chain") val chain: String,
-    @Column("retry_time") val retryTime: LocalDateTime,
-    var delay: Long,
-    @Column("error_delay") var errorDelay: Long
+    val chain: String,
+    val retryTime: LocalDateTime,
+    val delay: Long,
+    val errorDelay: Long,
+    @Id val id: Long? = null
 )
--- scheduler/src/main/kotlin/co/nilin/opex/chainscan/scheduler/spi/ChainLoader.kt
+++
@@ -1,9 +0,0 @@
-package co.nilin.opex.chainscan.scheduler.spi
-
-import co.nilin.opex.chainscan.scheduler.po.Chain
-
-interface ChainLoader {
-    suspend fun addChain(name: String, addressType: String): Chain
-    suspend fun fetchAllChains(): List<Chain>
-    suspend fun fetchChainInfo(chain: String): Chain
-}
