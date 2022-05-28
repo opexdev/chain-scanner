6df42288f8098ad5e5e73bde9cf21c3a396b9612
ebrahimmfadae <ebrahimmfadae@gmail.com>
2022-05-28 20:29:13 +0430

    eth-scan: Fix compile error

--- eth-scan/src/test/kotlin/co/nilin/opex/chainscan/eth/impl/ChainServiceTest.kt
+++ eth-scan/src/test/kotlin/co/nilin/opex/chainscan/eth/impl/ChainServiceTest.kt
@@ -1,25 +1,22 @@
 package co.nilin.opex.chainscan.eth.impl
 
-import co.nilin.opex.chainscan.core.spi.Decoder
 import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
 import org.web3j.protocol.Web3j
-import org.web3j.protocol.core.methods.response.EthBlock
 import org.web3j.protocol.core.methods.response.EthBlockNumber
 import java.math.BigInteger
 
 private class ChainServiceTest {
     private val web3j: Web3j = mockk()
-    private val decoder: Decoder<EthBlock.TransactionObject> = mockk()
-    private val chainService: ChainService = ChainService(web3j, decoder)
+    private val chainService: ChainService = ChainService(web3j)
 
     @Test
     fun given_when_then(): Unit = runBlocking {
         every { web3j.ethBlockNumber() } returns mockk {
             every { send() } returns EthBlockNumber().also { it.result = "0" }
         }
-        chainService.fetchAndConvert("", BigInteger.ONE, BigInteger.ONE, emptyList())
+        chainService.getTransactions(BigInteger.ONE, BigInteger.ONE)
     }
 }
