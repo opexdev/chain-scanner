226fa48f3d5fed389fe304db62a470d23e21cb3e
metalicn20 <metalicn20@gmail.com>
2022-05-28 11:51:40 +0430

    scheduler: Implement real webhook logic

--- scheduler/src/main/kotlin/co/nilin/opex/chainscan/scheduler/impl/WebhookCallerImpl.kt
+++ scheduler/src/main/kotlin/co/nilin/opex/chainscan/scheduler/impl/WebhookCallerImpl.kt
@@ -2,12 +2,18 @@ package co.nilin.opex.chainscan.scheduler.impl
 
 import co.nilin.opex.chainscan.scheduler.api.WebhookCaller
 import co.nilin.opex.chainscan.scheduler.po.Transfer
-import org.slf4j.LoggerFactory
+import kotlinx.coroutines.reactive.awaitFirst
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToMono
+import java.net.URI
 
-class WebhookCallerImpl : WebhookCaller {
-    private val logger = LoggerFactory.getLogger(WebhookCallerImpl::class.java)
-
+class WebhookCallerImpl(private val webClient: WebClient) : WebhookCaller {
     override suspend fun callWebhook(url: String, data: List<Transfer>) {
-        logger.info("Trigger {}", url)
+        webClient.post()
+            .uri(URI.create(url))
+            .retrieve()
+            .onStatus({ t -> t.isError }, { it.createException() })
+            .bodyToMono<Void>()
+            .awaitFirst()
     }
 }
