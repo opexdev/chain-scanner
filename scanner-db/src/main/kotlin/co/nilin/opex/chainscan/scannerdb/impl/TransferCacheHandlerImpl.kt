65939df70d1b26f0848e75c055a0ac7d2ad439e1
metalicn20 <metalicn20@gmail.com>
2022-05-28 11:33:05 +0430

    scanner-db: Implement transfer cache interface

--- scanner-db/src/main/kotlin/co/nilin/opex/chainscan/scannerdb/impl/TransferCacheHandlerImpl.kt
+++ scanner-db/src/main/kotlin/co/nilin/opex/chainscan/scannerdb/impl/TransferCacheHandlerImpl.kt
@@ -1,19 +1,50 @@
 package co.nilin.opex.chainscan.scannerdb.impl
 
 import co.nilin.opex.chainscan.core.model.Transfer
+import co.nilin.opex.chainscan.core.model.Wallet
 import co.nilin.opex.chainscan.core.spi.TransferCacheHandler
+import co.nilin.opex.chainscan.scannerdb.model.TransferModel
+import co.nilin.opex.chainscan.scannerdb.repository.TransferRepository
+import kotlinx.coroutines.reactor.awaitSingle
 import java.math.BigInteger
 
-class TransferCacheHandlerImpl : TransferCacheHandler {
+class TransferCacheHandlerImpl(
+    private val transferRepository: TransferRepository
+) : TransferCacheHandler {
     override suspend fun saveTransfers(transfers: List<Transfer>) {
-        TODO("Not yet implemented")
+        transferRepository.saveAll(transfers.map {
+            TransferModel(
+                it.txHash,
+                it.blockNumber,
+                it.from.address,
+                it.from.memo,
+                it.to.address,
+                it.to.memo,
+                it.isTokenTransfer,
+                it.amount,
+                it.chain,
+                it.tokenAddress
+            )
+        })
     }
 
     override suspend fun getTransfers(tokenAddresses: List<String>): List<Transfer> {
-        TODO("Not yet implemented")
+        return transferRepository.findByTokenAddress(tokenAddresses).map {
+            Transfer(
+                it.txHash,
+                it.blockNumber,
+                Wallet(it.fromAddress, it.fromMemo),
+                Wallet(it.toAddress, it.toAddress),
+                it.isTokenTransfer,
+                it.amount,
+                it.chain,
+                it.tokenAddress,
+                it.id!!
+            )
+        }.collectList().awaitSingle()
     }
 
     override suspend fun clearCache(blockNumber: BigInteger) {
-        TODO("Not yet implemented")
+        transferRepository.clearCache(blockNumber)
     }
 }
--- scanner-db/src/main/kotlin/co/nilin/opex/chainscan/scannerdb/repository/TransferRepository.kt
+++ scanner-db/src/main/kotlin/co/nilin/opex/chainscan/scannerdb/repository/TransferRepository.kt
@@ -1,8 +1,17 @@
 package co.nilin.opex.chainscan.scannerdb.repository
 
 import co.nilin.opex.chainscan.scannerdb.model.TransferModel
+import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.stereotype.Repository
+import reactor.core.publisher.Flux
+import java.math.BigInteger
 
 @Repository
-interface TransferRepository : ReactiveCrudRepository<TransferModel, Long>
+interface TransferRepository : ReactiveCrudRepository<TransferModel, Long> {
+    @Query("select * from transfers where token_address in (:tokenAddresses)")
+    fun findByTokenAddress(tokenAddresses: List<String>): Flux<TransferModel>
+
+    @Query("delete from transfers where block_number <= :blockNumber")
+    fun clearCache(blockNumber: BigInteger): Flux<TransferModel>
+}
