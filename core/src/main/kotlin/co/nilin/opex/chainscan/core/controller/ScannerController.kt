eccfec262c43a281b7833c820cb80bfdf58ca559
metalicn20 <metalicn20@gmail.com>
2022-05-28 13:11:47 +0430

    core: Implement fetch block number logic

--- core/src/main/kotlin/co/nilin/opex/chainscan/core/controller/ScannerController.kt
+++ core/src/main/kotlin/co/nilin/opex/chainscan/core/controller/ScannerController.kt
@@ -2,17 +2,14 @@ package co.nilin.opex.chainscan.core.controller
 
 import co.nilin.opex.chainscan.core.model.Transfer
 import co.nilin.opex.chainscan.core.service.ChainSyncService
-import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 import java.math.BigInteger
 
 @RestController
-class ScannerController(private val chainSyncService: ChainSyncService) {
-    private val logger = LoggerFactory.getLogger(ScannerController::class.java)
-
+class ScannerController(private val chainSyncService: ChainSyncService<*>) {
     @PostMapping("/transfers")
-    suspend fun getTransfers(start: BigInteger, end: BigInteger): List<Transfer> {
+    suspend fun getTransfers(start: BigInteger?, end: BigInteger?): List<Transfer> {
         return chainSyncService.getTransfers(start, end)
     }
 }
--- core/src/main/kotlin/co/nilin/opex/chainscan/core/service/ChainSyncService.kt
+++ core/src/main/kotlin/co/nilin/opex/chainscan/core/service/ChainSyncService.kt
@@ -1,10 +1,7 @@
 package co.nilin.opex.chainscan.core.service
 
 import co.nilin.opex.chainscan.core.model.Transfer
-import co.nilin.opex.chainscan.core.spi.Decoder
-import co.nilin.opex.chainscan.core.spi.FetchTransaction
-import co.nilin.opex.chainscan.core.spi.TransferCacheHandler
-import co.nilin.opex.chainscan.core.spi.WatchListHandler
+import co.nilin.opex.chainscan.core.spi.*
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import java.math.BigInteger
@@ -13,18 +10,22 @@ class ChainSyncService<T>(
     @Value("\$chain-name") private val chainName: String,
     private val fetchTransaction: FetchTransaction<T>,
     private val decoder: Decoder<T>,
+    private val getBlockNumber: GetBlockNumber,
     private val watchListHandler: WatchListHandler,
     private val transferCacheHandler: TransferCacheHandler,
 ) {
     private val logger = LoggerFactory.getLogger(ChainSyncService::class.java)
 
-    suspend fun getTransfers(start: BigInteger, end: BigInteger): List<Transfer> {
+    suspend fun getTransfers(start: BigInteger?, end: BigInteger?): List<Transfer> {
+        val actualStart = start ?: getBlockNumber.invoke()
+        val actualEnd = (end ?: getBlockNumber.invoke()).takeIf { it > actualStart } ?: actualStart
         val tokens = watchListHandler.findAll().map { impl -> impl.address }.toList()
-        logger.info("Syncing for: $chainName - Block: $start")
+        logger.info("Syncing for: $chainName - Block: $actualStart")
         val cached = transferCacheHandler.getTransfers(tokens)
         val notCachedStartBlock =
-            cached.filter { it.blockNumber >= start }.maxOfOrNull { it.blockNumber }?.plus(BigInteger.ONE) ?: start
-        val response = fetchTransaction.getTransactions(notCachedStartBlock, end)
+            cached.filter { it.blockNumber >= actualStart }.maxOfOrNull { it.blockNumber }?.plus(BigInteger.ONE)
+                ?: actualStart
+        val response = fetchTransaction.getTransactions(notCachedStartBlock, actualEnd)
         val transfers = response.map { decoder.invoke(it) }.filter {
             !it.isTokenTransfer || tokens.contains(it.tokenAddress)
         }
