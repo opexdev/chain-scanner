0fb3c2b3da9ff561c8069f49e167f109644de059
ebrahimmfadae <ebrahimmfadae@gamil.com>
2022-05-30 13:59:58 +0430

    scheduler-app: Implement retry handler

--- scheduler-app/src/main/kotlin/co/nilin/opex/chainscan/scheduler/impl/ChainSyncRetryHandlerImpl.kt
+++ scheduler-app/src/main/kotlin/co/nilin/opex/chainscan/scheduler/impl/ChainSyncRetryHandlerImpl.kt
@@ -1,21 +1,43 @@
 package co.nilin.opex.chainscan.scheduler.impl
 
 import co.nilin.opex.chainscan.scheduler.api.ChainSyncRetryHandler
+import co.nilin.opex.chainscan.scheduler.model.ChainSyncRetryModel
 import co.nilin.opex.chainscan.scheduler.po.ChainSyncRetry
 import co.nilin.opex.chainscan.scheduler.repository.ChainSyncRetryRepository
+import kotlinx.coroutines.reactor.awaitSingle
 import org.springframework.stereotype.Component
 
 @Component
 class ChainSyncRetryHandlerImpl(
     private val chainSyncRetryRepository: ChainSyncRetryRepository
 ) : ChainSyncRetryHandler {
-    private val maxRetry = 5
-
     override suspend fun save(chainSyncRetry: ChainSyncRetry) {
-        TODO("Not yet implemented")
+        chainSyncRetryRepository.save(
+            ChainSyncRetryModel(
+                chainSyncRetry.chain,
+                chainSyncRetry.startBlock,
+                chainSyncRetry.endBlock,
+                chainSyncRetry.retries,
+                chainSyncRetry.synced,
+                chainSyncRetry.giveUp,
+                chainSyncRetry.error,
+                chainSyncRetry.id
+            )
+        ).awaitSingle()
     }
 
     override suspend fun findAllActive(chainName: String): List<ChainSyncRetry> {
-        TODO("Not yet implemented")
+        return chainSyncRetryRepository.finByChainNameWhereNoGiveUp(chainName).map {
+            ChainSyncRetry(
+                it.chain,
+                it.startBlock,
+                it.endBlock,
+                it.retries,
+                it.synced,
+                it.giveUp,
+                it.error,
+                it.id
+            )
+        }.collectList().awaitSingle()
     }
 }
--- scheduler-app/src/main/kotlin/co/nilin/opex/chainscan/scheduler/repository/ChainSyncRetryRepository.kt
+++ scheduler-app/src/main/kotlin/co/nilin/opex/chainscan/scheduler/repository/ChainSyncRetryRepository.kt
@@ -3,6 +3,9 @@ package co.nilin.opex.chainscan.scheduler.repository
 import co.nilin.opex.chainscan.scheduler.model.ChainSyncRetryModel
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.stereotype.Repository
+import reactor.core.publisher.Flux
 
 @Repository
-interface ChainSyncRetryRepository : ReactiveCrudRepository<ChainSyncRetryModel, Long>
+interface ChainSyncRetryRepository : ReactiveCrudRepository<ChainSyncRetryModel, Long> {
+    fun finByChainNameWhereNoGiveUp(chainName: String): Flux<ChainSyncRetryModel>
+}
