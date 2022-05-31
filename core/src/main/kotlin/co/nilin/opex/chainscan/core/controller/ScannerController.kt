6240657327ff1d963ee52221e5d5e538f2979ae4
ebrahimmfadae <ebrahimmfadae@gamil.com>
2022-05-31 16:31:20 +0430

    core: Implement /block-number controller

--- core/src/main/kotlin/co/nilin/opex/chainscan/core/controller/ScannerController.kt
+++ core/src/main/kotlin/co/nilin/opex/chainscan/core/controller/ScannerController.kt
@@ -2,14 +2,20 @@ package co.nilin.opex.chainscan.core.controller
 
 import co.nilin.opex.chainscan.core.model.Transfer
 import co.nilin.opex.chainscan.core.service.ChainSyncService
+import co.nilin.opex.chainscan.core.spi.GetBlockNumber
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import java.math.BigInteger
 
 @RestController
-class ScannerController(private val chainSyncService: ChainSyncService<*>) {
+class ScannerController(private val chainSyncService: ChainSyncService<*>, private val getBlockNumber: GetBlockNumber) {
     @GetMapping("/transfers")
     suspend fun getTransfers(blockNumber: BigInteger?): List<Transfer> {
         return chainSyncService.getTransfers(blockNumber)
+    }
+
+    @GetMapping("/block-number")
+    suspend fun getBlockNumber(): BigInteger {
+        return getBlockNumber.invoke()
     }
 }
