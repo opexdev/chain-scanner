b9f9723198e6aee7e160d9e56e1a6c58d4f04812
ebrahimmfadae <ebrahimmfadae@gmail.com>
2022-06-01 21:19:37 +0430

    Refactor bitcoin-app module

--- bitcoin-app/src/main/kotlin/co/nilin/opex/chainscan/bitcoin/proxy/GetBlockProxy.kt
+++ bitcoin-app/src/main/kotlin/co/nilin/opex/chainscan/bitcoin/proxy/GetBlockProxy.kt
@@ -15,15 +15,15 @@ import org.springframework.web.reactive.function.client.WebClient
 @Component
 class GetBlockProxy(
     private val webClient: WebClient,
-    @Value("\${app.rest-endpoint}")
+    @Value("\${app.rest-api.endpoint}")
     private val endpoint: String,
-    @Value("\${app.api-key}")
+    @Value("\${app.rest-api.api-key}")
     private val apiKey: String
 ) {
     private val logger: Logger by LoggerDelegate()
 
     suspend fun getInfo(): ChainInfoResponse? {
-        logger.info("Fetching chain info")
+        logger.trace("Fetching chain info")
         return webClient.get()
             .uri("$endpoint/chaininfo.json")
             .accept(MediaType.APPLICATION_JSON)
@@ -36,7 +36,7 @@ class GetBlockProxy(
     }
 
     suspend fun getBlockHash(height: Long): String {
-        logger.info("Fetching block hash of $height")
+        logger.trace("Fetching block hash of $height")
         return webClient.get()
             .uri("$endpoint/blockhashbyheight/${height}.json")
             .accept(MediaType.APPLICATION_JSON)
@@ -45,12 +45,11 @@ class GetBlockProxy(
             .retrieve()
             .onStatus({ t -> t.isError }, { it.createException() })
             .bodyToMono(BlockHashResponse::class.java)
-            .awaitSingleOrNull()
-            ?.blockHash ?: ""
+            .awaitSingleOrNull()?.blockHash ?: ""
     }
 
     suspend fun getBlockData(hash: String): BlockResponse? {
-        logger.info("Fetching block data of $hash")
+        logger.trace("Fetching block data of $hash")
         return webClient.get()
             .uri("$endpoint/block/${hash}.json")
             .accept(MediaType.APPLICATION_JSON)
--- bitcoin-app/src/main/kotlin/co/nilin/opex/chainscan/bitcoin/service/BitcoinDataDecoder.kt
+++ bitcoin-app/src/main/kotlin/co/nilin/opex/chainscan/bitcoin/service/BitcoinDataDecoder.kt
@@ -4,25 +4,27 @@ import co.nilin.opex.chainscan.bitcoin.data.BlockResponse
 import co.nilin.opex.chainscan.core.model.Transfer
 import co.nilin.opex.chainscan.core.model.Wallet
 import co.nilin.opex.chainscan.core.spi.DataDecoder
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.*
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 @Component
-class BitcoinDataDecoder(@Value("\${app.chain-name}") private val chainName: String) : DataDecoder<BlockResponse> {
+class BitcoinDataDecoder(@Value("\${app.chain.name}") private val chainName: String) : DataDecoder<BlockResponse> {
     override suspend fun decode(input: BlockResponse): List<Transfer> {
         return input.tx.flatMap { tx ->
-            tx.vout.filter {
+            tx.vout.asFlow().flowOn(Dispatchers.IO).filter {
                 !it.scriptPubKey?.address.isNullOrBlank()
             }.map { v ->
                 Transfer(
-                    "${tx.hash}_${v.scriptPubKey?.hex}",
+                    "${tx.hash}_${v.n}",
                     input.height.toBigInteger(),
                     Wallet(v.scriptPubKey!!.address!!),
                     false,
                     v.value,
                     chainName
                 )
-            }
+            }.buffer().toList()
         }
     }
 }
