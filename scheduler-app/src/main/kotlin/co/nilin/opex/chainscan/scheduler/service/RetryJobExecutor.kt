8e7f150f13766557b18ac7a6d61eea37192a8872
ebrahimmfadae <ebrahimmfadae@gamil.com>
2022-06-01 14:31:59 +0430

    scheduler-app: Rename jobs

--- scheduler-app/src/main/kotlin/co/nilin/opex/chainscan/scheduler/service/RetryJobExecutor.kt
+++ scheduler-app/src/main/kotlin/co/nilin/opex/chainscan/scheduler/service/RetryJobExecutor.kt
@@ -4,7 +4,6 @@ import co.nilin.opex.chainscan.scheduler.api.*
 import co.nilin.opex.chainscan.scheduler.po.ChainSyncSchedule
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.supervisorScope
-import kotlinx.coroutines.withTimeoutOrNull
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
@@ -18,23 +17,21 @@ class RetryJobExecutor(
     @Value("\${app.on-sync-webhook-url}") private val onSyncWebhookUrl: String
 ) : JobExecutor {
     override suspend fun execute(sch: ChainSyncSchedule) {
-        withTimeoutOrNull(sch.timeout) {
-            val chain = chainScannerHandler.getScannersByName(sch.chainName).first()
-            val chainSyncRetries = chainSyncRetryHandler.findAllActive(sch.chainName)
-            supervisorScope {
-                chainSyncRetries.forEach { retry ->
-                    launch {
-                        runCatching {
-                            val response = scannerProxy.getTransfers(chain.url, retry.blockNumber)
-                            webhookCaller.callWebhook(onSyncWebhookUrl, response.transfers)
-                            chainSyncRecordHandler.lastSyncRecord(sch.chainName)
-                        }.onFailure {
-                            val retries = retry.retries + 1
-                            chainSyncRetryHandler.save(retry.copy(retries = retries, giveUp = retries >= 5))
-                        }.onSuccess {
-                            val retries = retry.retries + 1
-                            chainSyncRetryHandler.save(retry.copy(retries = retries, synced = true))
-                        }
+        val chain = chainScannerHandler.getScannersByName(sch.chainName).first()
+        val chainSyncRetries = chainSyncRetryHandler.findAllActive(sch.chainName)
+        supervisorScope {
+            chainSyncRetries.forEach { retry ->
+                launch {
+                    runCatching {
+                        val response = scannerProxy.getTransfers(chain.url, retry.blockNumber)
+                        webhookCaller.callWebhook(onSyncWebhookUrl, response.transfers)
+                        chainSyncRecordHandler.lastSyncRecord(sch.chainName)
+                    }.onFailure {
+                        val retries = retry.retries + 1
+                        chainSyncRetryHandler.save(retry.copy(retries = retries, giveUp = retries >= 5))
+                    }.onSuccess {
+                        val retries = retry.retries + 1
+                        chainSyncRetryHandler.save(retry.copy(retries = retries, synced = true))
                     }
                 }
             }
