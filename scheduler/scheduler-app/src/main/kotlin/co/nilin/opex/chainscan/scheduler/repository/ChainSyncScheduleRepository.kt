3709948b8c444d97f224621505c583ac41f5ebd5
ebrahimmfadae <ebrahimmfadae@gmail.com>
2022-06-01 21:40:18 +0430

    Refactor scheduler module

--- scheduler/scheduler-app/src/main/kotlin/co/nilin/opex/chainscan/scheduler/repository/ChainSyncScheduleRepository.kt
+++ scheduler/scheduler-app/src/main/kotlin/co/nilin/opex/chainscan/scheduler/repository/ChainSyncScheduleRepository.kt
@@ -5,7 +5,6 @@ import kotlinx.coroutines.flow.Flow
 import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.stereotype.Repository
-import reactor.core.publisher.Mono
 import java.time.LocalDateTime
 
 @Repository
