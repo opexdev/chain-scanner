49163e6f4c0e46b81f97230b1bb66d15d52307e1
ebrahimmfadae <ebrahimmfadae@gmail.com>
2022-06-02 14:51:10 +0430

    scheduler-app: Improve jobs

--- scheduler/scheduler-app/src/main/kotlin/co/nilin/opex/chainscan/scheduler/jobs/RetryFailedSyncsScheduledJob.kt
+++ scheduler/scheduler-app/src/main/kotlin/co/nilin/opex/chainscan/scheduler/jobs/RetryFailedSyncsScheduledJob.kt
@@ -1,11 +1,12 @@
 package co.nilin.opex.chainscan.scheduler.jobs
 
-import co.nilin.opex.chainscan.scheduler.core.po.ChainSyncRecord
+import co.nilin.opex.chainscan.scheduler.core.po.ChainScanner
+import co.nilin.opex.chainscan.scheduler.core.po.ChainSyncRetry
 import co.nilin.opex.chainscan.scheduler.core.po.ChainSyncSchedule
 import co.nilin.opex.chainscan.scheduler.core.spi.*
 import co.nilin.opex.chainscan.scheduler.utils.LoggerDelegate
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.supervisorScope
 import org.slf4j.Logger
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
@@ -25,47 +26,56 @@ class RetryFailedSyncsScheduledJob(
     private val logger: Logger by LoggerDelegate()
 
     override suspend fun execute(sch: ChainSyncSchedule) {
-        val chain = chainScannerHandler.getScannersByName(sch.chainName).first()
+        val chainScanner = chainScannerHandler.getScannersByName(sch.chainName).first()
         val chainSyncRetries = chainSyncRetryHandler.findAllActive(sch.chainName)
-        supervisorScope {
-            val blockRange = chainSyncRetries.chunked(chain.maxBlockRange).firstOrNull()
-            blockRange?.forEach { retry ->
-                launch {
-                    logger.trace("Retry block sync on blockNumber: ${retry.blockNumber}")
-                    runCatching {
-                        val response = scannerProxy.getTransfers(chain.url, retry.blockNumber)
-                        webhookCaller.callWebhook(chain.chainName, response)
-                    }.onFailure { e ->
-                        val retries = retry.retries + 1
-                        chainSyncRetryHandler.save(retry.copy(retries = retries, giveUp = retries >= sch.maxRetries))
-                        when (e) {
-                            is WebClientResponseException -> e.takeIf { it.statusCode == HttpStatus.TOO_MANY_REQUESTS }
-                                ?.run { sch.nextSchedule(chain.delayOnRateLimit.toLong()) }
-                                ?: sch.nextSchedule(sch.errorDelay)
-                            is Exception -> sch.nextSchedule(sch.errorDelay)
-                            else -> sch.nextSchedule(sch.delay)
-                        }
-                    }.onSuccess {
-                        val lastSyncRecord = chainSyncRecordHandler.lastSyncRecord(sch.chainName) ?: ChainSyncRecord(
-                            sch.chainName,
-                            LocalDateTime.now(),
-                            retry.blockNumber
-                        )
-                        chainSyncRecordHandler.saveSyncRecord(
-                            lastSyncRecord.copy(
-                                syncTime = LocalDateTime.now(),
-                                blockNumber = retry.blockNumber
-                            )
-                        )
-                        val retries = retry.retries + 1
-                        chainSyncRetryHandler.save(retry.copy(retries = retries, synced = true))
+        runCatching {
+            coroutineScope {
+                val blockRange = chainSyncRetries.chunked(chainScanner.maxBlockRange).firstOrNull()
+                blockRange?.forEach { retry ->
+                    launch {
+                        logger.trace("Retry block sync on blockNumber: ${retry.blockNumber}")
+                        fetch(chainScanner, retry, sch)
                     }
                 }
+            }
+        }.onFailure { e ->
+            when (e) {
+                is WebClientResponseException -> e.takeIf { it.statusCode == HttpStatus.TOO_MANY_REQUESTS }
+                    ?.run { sch.enqueueNextSchedule(chainScanner.delayOnRateLimit.toLong()) }
+                    ?: sch.enqueueNextSchedule(sch.errorDelay)
+                else -> sch.enqueueNextSchedule(sch.errorDelay)
             }
         }
     }
 
-    private suspend fun ChainSyncSchedule.nextSchedule(nextTimeDiff: Long) {
+    private suspend fun fetch(
+        chain: ChainScanner,
+        retry: ChainSyncRetry,
+        sch: ChainSyncSchedule
+    ) {
+        runCatching {
+            val response = scannerProxy.getTransfers(chain.url, retry.blockNumber)
+            webhookCaller.callWebhook(chain.chainName, response)
+        }.onFailure { e ->
+            val retries = retry.retries + 1
+            chainSyncRetryHandler.save(
+                retry.copy(
+                    retries = retries,
+                    giveUp = retries >= sch.maxRetries,
+                    error = e.message
+                )
+            )
+            when (e) {
+                is WebClientResponseException -> e.takeUnless { it.statusCode == HttpStatus.TOO_MANY_REQUESTS }
+                    ?: throw e
+            }
+        }.onSuccess {
+            val retries = retry.retries + 1
+            chainSyncRetryHandler.save(retry.copy(retries = retries, synced = true))
+        }
+    }
+
+    private suspend fun ChainSyncSchedule.enqueueNextSchedule(nextTimeDiff: Long) {
         val retryTime = LocalDateTime.now().plus(nextTimeDiff, ChronoUnit.SECONDS)
         chainSyncSchedulerHandler.save(copy(retryTime = retryTime))
     }
--- scheduler/scheduler-app/src/main/kotlin/co/nilin/opex/chainscan/scheduler/jobs/SyncLatestTransfersScheduledJob.kt
+++ scheduler/scheduler-app/src/main/kotlin/co/nilin/opex/chainscan/scheduler/jobs/SyncLatestTransfersScheduledJob.kt
@@ -6,8 +6,8 @@ import co.nilin.opex.chainscan.scheduler.core.po.ChainSyncRetry
 import co.nilin.opex.chainscan.scheduler.core.po.ChainSyncSchedule
 import co.nilin.opex.chainscan.scheduler.core.spi.*
 import co.nilin.opex.chainscan.scheduler.utils.LoggerDelegate
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.supervisorScope
 import org.slf4j.Logger
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
@@ -28,59 +28,72 @@ class SyncLatestTransfersScheduledJob(
     private val logger: Logger by LoggerDelegate()
 
     override suspend fun execute(sch: ChainSyncSchedule) {
-        val chain = chainScannerHandler.getScannersByName(sch.chainName).first()
-        val currentBlockNumber = scannerProxy.getBlockNumber(chain.url)
-        val head = currentBlockNumber - sch.confirmations.toBigInteger()
-        val startBlockNumber =
-            chainSyncRecordHandler.lastSyncedBlockedNumber(sch.chainName)?.plus(BigInteger.ONE)
-                ?: head
-        val endBlockNumber = head.max(startBlockNumber)
-        val blockRange = startBlockNumber.toLong()..endBlockNumber.toLong()
-        logger.trace("Fetch transfers on block range: $startBlockNumber - $endBlockNumber")
+        val chainScanner = chainScannerHandler.getScannersByName(sch.chainName).first()
+        val blockRange = calculateBlockRange(chainScanner, sch.confirmations)
+        logger.trace("Fetch transfers on block range: ${blockRange.first} - ${blockRange.last}")
         runCatching {
-            supervisorScope {
-                val br = blockRange.chunked(chain.maxBlockRange).firstOrNull()
+            coroutineScope {
+                val br = blockRange.chunked(chainScanner.maxBlockRange).firstOrNull()
                 br?.forEach { bn ->
-                    launch { fetch(chain, bn.toBigInteger(), sch) }
+                    launch { fetch(chainScanner, bn.toBigInteger(), sch) }
                 }
             }
         }.onFailure { e ->
             when (e) {
                 is WebClientResponseException -> e.takeIf { it.statusCode == HttpStatus.TOO_MANY_REQUESTS }
-                    ?.run { sch.nextSchedule(chain.delayOnRateLimit.toLong()) } ?: sch.nextSchedule(sch.errorDelay)
-                is Exception -> sch.nextSchedule(sch.errorDelay)
-                else -> sch.nextSchedule(sch.delay)
+                    ?.run { sch.enqueueNextSchedule(chainScanner.delayOnRateLimit.toLong()) }
+                    ?: sch.enqueueNextSchedule(sch.errorDelay)
+                else -> sch.enqueueNextSchedule(sch.errorDelay)
             }
         }.onSuccess {
-            sch.nextSchedule(sch.delay)
+            sch.enqueueNextSchedule(sch.delay)
         }
     }
 
-    private suspend fun ChainSyncSchedule.nextSchedule(nextTimeDiff: Long) {
+    private suspend fun calculateBlockRange(chainScanner: ChainScanner, confirmations: Int): LongRange {
+        val chainHeadBlock = scannerProxy.getBlockNumber(chainScanner.url)
+        val confirmedBlock = chainHeadBlock - confirmations.toBigInteger()
+        val lastSyncedBlock = chainSyncRecordHandler.lastSyncedBlockedNumber(chainScanner.chainName)
+        val startBlock = lastSyncedBlock?.plus(BigInteger.ONE) ?: confirmedBlock
+        val endBlock = confirmedBlock.min(startBlock + chainScanner.maxBlockRange.toBigInteger())
+        return startBlock.toLong()..endBlock.toLong()
+    }
+
+    private suspend fun ChainSyncSchedule.enqueueNextSchedule(nextTimeDiff: Long) {
         val retryTime = LocalDateTime.now().plus(nextTimeDiff, ChronoUnit.SECONDS)
         chainSyncSchedulerHandler.save(copy(retryTime = retryTime))
     }
 
-    private suspend fun fetch(chain: ChainScanner, blockNumber: BigInteger, sch: ChainSyncSchedule) {
+    private suspend fun fetch(chainScanner: ChainScanner, blockNumber: BigInteger, sch: ChainSyncSchedule) {
         runCatching {
-            val response = scannerProxy.getTransfers(chain.url, blockNumber)
-            webhookCaller.callWebhook(chain.chainName, response)
+            val response = scannerProxy.getTransfers(chainScanner.url, blockNumber)
+            webhookCaller.callWebhook(chainScanner.chainName, response)
         }.onFailure { e ->
-            val chainSyncRetry = chainSyncRetryHandler.findByChainAndBlockNumber(chain.chainName, blockNumber)
+            val chainSyncRetry = chainSyncRetryHandler.findByChainAndBlockNumber(chainScanner.chainName, blockNumber)
             chainSyncRetry?.let { chainSyncRetryHandler.save(it.copy(error = e.message)) }
                 ?: chainSyncRetryHandler.save(
                     ChainSyncRetry(
-                        chain.chainName,
+                        chainScanner.chainName,
                         blockNumber,
                         error = e.message,
                         maxRetries = sch.maxRetries
                     )
                 )
+            when (e) {
+                is WebClientResponseException -> e.takeUnless { it.statusCode == HttpStatus.TOO_MANY_REQUESTS }
+                    ?: throw e
+            }
         }.also {
-            val record = chainSyncRecordHandler.lastSyncRecord(chain.chainName)
+            val record = chainSyncRecordHandler.lastSyncRecord(chainScanner.chainName) ?: ChainSyncRecord(
+                chainScanner.chainName,
+                LocalDateTime.now(),
+                blockNumber
+            )
             chainSyncRecordHandler.saveSyncRecord(
-                record?.copy(syncTime = LocalDateTime.now(), blockNumber = blockNumber)
-                    ?: ChainSyncRecord(chain.chainName, LocalDateTime.now(), blockNumber)
+                record.copy(
+                    syncTime = LocalDateTime.now(),
+                    blockNumber = blockNumber
+                )
             )
         }.getOrThrow()
     }
