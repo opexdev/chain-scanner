964895e20ae2e45e75ee79a9e735ea063dac22ad
ebrahimmfadae <ebrahimmfadae@gmail.com>
2022-06-09 12:01:42 +0430

    scheduler: Improve error handling

--- scheduler/scheduler-app/src/main/kotlin/co/nilin/opex/chainscanner/scheduler/schedule/tasks/RetryFailedSyncs.kt
+++ scheduler/scheduler-app/src/main/kotlin/co/nilin/opex/chainscanner/scheduler/schedule/tasks/RetryFailedSyncs.kt
@@ -5,10 +5,12 @@ import co.nilin.opex.chainscanner.scheduler.core.spi.ChainScannerHandler
 import co.nilin.opex.chainscanner.scheduler.core.spi.ChainSyncRetryHandler
 import co.nilin.opex.chainscanner.scheduler.core.spi.ChainSyncSchedulerHandler
 import co.nilin.opex.chainscanner.scheduler.core.spi.ScheduleTask
+import co.nilin.opex.chainscanner.scheduler.exceptions.ScannerConnectException
 import co.nilin.opex.chainscanner.scheduler.service.GetTransfersSubTask
 import co.nilin.opex.chainscanner.scheduler.utils.LoggerDelegate
-import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.supervisorScope
 import org.slf4j.Logger
 import org.springframework.stereotype.Service
 
@@ -26,13 +28,16 @@ class RetryFailedSyncs(
         val chainSyncRetries = chainSyncRetryHandler.findAllActive(sch.chainName)
         val blockRange = chainSyncRetries.take(chainScanner.maxBlockRange)
         runCatching {
-            coroutineScope {
+            supervisorScope {
                 blockRange.forEach { chainSyncRetry ->
                     launch {
                         logger.debug("Retry block sync on blockNumber: ${chainSyncRetry.blockNumber}")
                         getTransfersSubTask.fetch(sch, chainScanner, chainSyncRetry.blockNumber).onSuccess {
                             chainSyncRetryHandler.markAsSynced(chainSyncRetry)
                             logger.info("Successfully retried block: ${chainSyncRetry.blockNumber}")
+                        }.onFailure {
+                            logger.error("Failed to sync block: ${chainSyncRetry.blockNumber} tries: ${chainSyncRetry.retries}/${chainSyncRetry.maxRetries}")
+                            if (it is ScannerConnectException) cancel(it.message ?: "Unknown")
                         }
                     }
                 }
--- scheduler/scheduler-app/src/main/kotlin/co/nilin/opex/chainscanner/scheduler/service/BlockRangeCalculatorImpl.kt
+++ scheduler/scheduler-app/src/main/kotlin/co/nilin/opex/chainscanner/scheduler/service/BlockRangeCalculatorImpl.kt
@@ -4,11 +4,14 @@ import co.nilin.opex.chainscanner.scheduler.api.BlockRangeCalculator
 import co.nilin.opex.chainscanner.scheduler.core.po.ChainScanner
 import co.nilin.opex.chainscanner.scheduler.core.spi.ChainSyncRecordHandler
 import co.nilin.opex.chainscanner.scheduler.core.spi.ScannerProxy
+import co.nilin.opex.chainscanner.scheduler.exceptions.RateLimitException
 import co.nilin.opex.chainscanner.scheduler.exceptions.ScannerConnectException
 import co.nilin.opex.chainscanner.scheduler.utils.LoggerDelegate
 import org.slf4j.Logger
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClientRequestException
+import org.springframework.web.reactive.function.client.WebClientResponseException
 import java.math.BigInteger
 import java.net.ConnectException
 
@@ -27,8 +30,13 @@ class BlockRangeCalculatorImpl(
         val endBlock = confirmedBlock.min(startBlock + chainScanner.maxBlockRange.toBigInteger())
         startBlock.toLong()..endBlock.toLong()
     }.onFailure { e ->
-        if (e is WebClientRequestException && e.isConnectionError) throw ScannerConnectException("Block range")
+        if (e is WebClientRequestException && e.isConnectionError)
+            throw ScannerConnectException("scannerProxy.getBlockNumber()")
+        if (e is WebClientResponseException && e.isTooManyRequests) throw RateLimitException()
     }.getOrThrow()
+
+    private val WebClientResponseException.isTooManyRequests: Boolean
+        get() = statusCode == HttpStatus.TOO_MANY_REQUESTS
 
     private val WebClientRequestException.isConnectionError: Boolean
         get() = mostSpecificCause is ConnectException
