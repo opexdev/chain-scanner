0b69c2c0f1cd21d868764899235aa2cb17bffd36
Ebrahim Hoseiny Fadae <ebrahimmfadae@gmail.com>
2022-07-03 17:17:24 +0430

    scanner-scheduler: Refactor error handlers (#52)

--- scheduler/scheduler-app/src/main/kotlin/co/nilin/opex/chainscanner/scheduler/schedule/tasks/RetryFailedSyncs.kt
+++ scheduler/scheduler-app/src/main/kotlin/co/nilin/opex/chainscanner/scheduler/schedule/tasks/RetryFailedSyncs.kt
@@ -1,10 +1,10 @@
 package co.nilin.opex.chainscanner.scheduler.schedule.tasks
 
+import co.nilin.opex.chainscanner.scheduler.core.po.ChainScanner
 import co.nilin.opex.chainscanner.scheduler.core.po.ChainSyncSchedule
 import co.nilin.opex.chainscanner.scheduler.core.spi.ChainScannerHandler
 import co.nilin.opex.chainscanner.scheduler.core.spi.ChainSyncRetryHandler
 import co.nilin.opex.chainscanner.scheduler.core.spi.ScheduleTask
-import co.nilin.opex.chainscanner.scheduler.exceptions.ScannerConnectException
 import co.nilin.opex.chainscanner.scheduler.service.GetTransfersSubTask
 import co.nilin.opex.chainscanner.scheduler.utils.LoggerDelegate
 import kotlinx.coroutines.cancel
@@ -21,9 +21,7 @@ class RetryFailedSyncs(
 ) : ScheduleTask {
     private val logger: Logger by LoggerDelegate()
 
-    override suspend fun execute(sch: ChainSyncSchedule) {
-        val chainScanner = chainScannerHandler.getScannersByName(sch.chainName).firstOrNull()
-            ?: throw IllegalStateException("No chain scanner found for chain: ${sch.chainName}")
+    override suspend fun execute(sch: ChainSyncSchedule, chainScanner: ChainScanner) {
         val chainSyncRetries = chainSyncRetryHandler.findAllActive(sch.chainName)
         val blockRange = chainSyncRetries.take(chainScanner.maxBlockRange)
         supervisorScope {
@@ -35,7 +33,7 @@ class RetryFailedSyncs(
                         logger.info("Successfully retried syncing for chain: ${sch.chainName} block: ${chainSyncRetry.blockNumber}")
                     }.onFailure {
                         logger.error("Failed to sync chain: ${sch.chainName} block: ${chainSyncRetry.blockNumber} tries: ${chainSyncRetry.retries}/${chainSyncRetry.maxRetries}")
-                        if (it is ScannerConnectException) cancel(it.message ?: "Unknown")
+                        cancel(it.message ?: "Unknown", it)
                     }
                 }
             }
--- scheduler/scheduler-app/src/main/kotlin/co/nilin/opex/chainscanner/scheduler/service/ScannerProxyImpl.kt
+++ scheduler/scheduler-app/src/main/kotlin/co/nilin/opex/chainscanner/scheduler/service/ScannerProxyImpl.kt
@@ -2,15 +2,19 @@ package co.nilin.opex.chainscanner.scheduler.service
 
 import co.nilin.opex.chainscanner.scheduler.core.po.Transfer
 import co.nilin.opex.chainscanner.scheduler.core.spi.ScannerProxy
-import co.nilin.opex.chainscanner.scheduler.utils.LoggerDelegate
+import co.nilin.opex.chainscanner.scheduler.exceptions.RateLimitException
+import co.nilin.opex.chainscanner.scheduler.exceptions.ScannerConnectException
 import kotlinx.coroutines.reactive.awaitFirst
 import kotlinx.coroutines.reactive.awaitFirstOrNull
-import org.slf4j.Logger
 import org.springframework.core.ParameterizedTypeReference
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClientRequestException
+import org.springframework.web.reactive.function.client.WebClientResponseException
 import java.math.BigInteger
+import java.net.ConnectException
 import java.net.URI
 import java.util.*
 
@@ -19,43 +23,67 @@ private inline fun <reified T : Any> parameterizedTypeReference(): Parameterized
 
 @Service
 class ScannerProxyImpl(private val webClient: WebClient) : ScannerProxy {
-    private val logger: Logger by LoggerDelegate()
-
     override suspend fun getTransfers(url: String, blockNumber: BigInteger?): List<Transfer> {
         val uri = URI.create("$url/transfers").normalize()
-        return webClient.get()
-            .uri {
-                val query = it.queryParamIfPresent("blockNumber", Optional.ofNullable(blockNumber)).build()
-                uri.resolve("$uri$query").normalize()
-            }
-            .accept(MediaType.APPLICATION_JSON)
-            .retrieve()
-            .onStatus({ t -> t.isError }, { it.createException() })
-            .bodyToMono(parameterizedTypeReference<List<Transfer>>())
-            .awaitFirst()
+        return runCatching {
+            webClient.get()
+                .uri {
+                    val query = it.queryParamIfPresent("blockNumber", Optional.ofNullable(blockNumber)).build()
+                    uri.resolve("$uri$query").normalize()
+                }
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .onStatus({ t -> t.isError }, { it.createException() })
+                .bodyToMono(parameterizedTypeReference<List<Transfer>>())
+                .awaitFirst()
+        }.onFailure {
+            if (it is WebClientRequestException && it.isConnectionError)
+                throw ScannerConnectException(uri.toString())
+            else if (it is WebClientResponseException && it.isTooManyRequests)
+                throw RateLimitException("Rate limit on uri: $uri")
+        }.getOrThrow()
     }
 
     override suspend fun getBlockNumber(url: String): BigInteger {
-        return webClient.get()
-            .uri { URI.create("$url/block-number").normalize() }
-            .accept(MediaType.APPLICATION_JSON)
-            .retrieve()
-            .onStatus({ t -> t.isError }, { it.createException() })
-            .bodyToMono(BigInteger::class.java)
-            .awaitFirst()
+        val uri = URI.create("$url/block-number").normalize()
+        return runCatching {
+            webClient.get()
+                .uri { uri }
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .onStatus({ t -> t.isError }, { it.createException() })
+                .bodyToMono(BigInteger::class.java)
+                .awaitFirst()
+        }.onFailure {
+            if (it is WebClientRequestException && it.isConnectionError)
+                throw ScannerConnectException(uri.toString())
+            else if (it is WebClientResponseException && it.isTooManyRequests)
+                throw RateLimitException("Rate limit on uri: $uri")
+        }.getOrThrow()
     }
 
     override suspend fun clearCache(url: String, blockNumber: BigInteger) {
         val uri = URI.create("$url/clear-cache").normalize()
-        webClient.delete()
-            .uri {
-                val query = it.queryParam("blockNumber", blockNumber).build()
-                uri.resolve("$uri$query").normalize()
-            }
-            .accept(MediaType.APPLICATION_JSON)
-            .retrieve()
-            .onStatus({ t -> t.isError }, { it.createException() })
-            .bodyToMono(parameterizedTypeReference<List<Transfer>>())
-            .awaitFirstOrNull()
+        runCatching {
+            webClient.delete()
+                .uri {
+                    val query = it.queryParam("blockNumber", blockNumber).build()
+                    uri.resolve("$uri$query").normalize()
+                }
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .onStatus({ t -> t.isError }, { it.createException() })
+                .bodyToMono(parameterizedTypeReference<List<Transfer>>())
+                .awaitFirstOrNull()
+        }.onFailure {
+            if (it is WebClientRequestException && it.isConnectionError)
+                throw ScannerConnectException(uri.toString())
+        }.getOrThrow()
     }
+
+    private val WebClientResponseException.isTooManyRequests: Boolean
+        get() = statusCode == HttpStatus.TOO_MANY_REQUESTS
+
+    private val WebClientRequestException.isConnectionError: Boolean
+        get() = mostSpecificCause is ConnectException
 }
